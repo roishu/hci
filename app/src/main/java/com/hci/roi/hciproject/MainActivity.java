5c3a8f4689cbb8ffc24cfc8dfa0578bc37e8bd3a
Roi <roishuk@gmail.com>
2017-06-03 14:56:36 +0300

    first commit

--- app/src/main/java/com/hci/roi/hciproject/MainActivity.java
+++ app/src/main/java/com/hci/roi/hciproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.hci.roi.hciproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cc.roi.aircc.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
